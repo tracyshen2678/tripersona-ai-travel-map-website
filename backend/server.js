4a66a1b208ab3fed841c0d9d18163ef27fe5053d
tracyshen2678 <tracyshen301@gmail.com>
2025-06-03 20:30:50 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_URL.split("@")[1],
-  api_key: process.env.CLOUDINARY_URL.split(":")[1].split("@")[0],
-  api_secret: process.env.CLOUDINARY_URL.split(":")[2],
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
